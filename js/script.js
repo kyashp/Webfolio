4d63ae05256c40428035cf0b3213116008acf839
Yash <154773174+kyashp@users.noreply.github.com>
2025-07-14 16:08:06 +0800

    Completed: Intro Section

--- js/script.js
+++ js/script.js
@@ -34,11 +34,60 @@ document.getElementById('main-logo').addEventListener('click',(e)=>{
     }
 });
 
+const burg= document.querySelector('.hamburger');
+
 function toggleMenu(){
-    const burg= document.querySelector('.hamburger');
     const navigation= document.querySelector('.nav');
 
     burg.classList.toggle('active');
     navigation.classList.toggle('active');
 }
 burg.addEventListener('click', toggleMenu());
+
+const translations=[
+  { text: "Hi, ", language: "English", dir: "ltr" },
+  { text: "வணக்கம், ", language: "Tamil", dir: "ltr" },
+  { text: "Hola, ", language: "Spanish", dir: "ltr" },
+  { text: "Salut, ", language: "French", dir: "ltr" },
+  { text: "Hallo, ", language: "German", dir: "ltr" },
+  { text: "Ciao, ", language: "Italian", dir: "ltr" },
+  { text: "こんにちは, ", language: "Japanese", dir: "ltr" },
+  { text: "안녕하세요, ", language: "Korean", dir: "ltr" },
+  { text: "你好, ", language: "Chinese (Simplified)", dir: "ltr" },
+  { text: "Привет, ", language: "Russian", dir: "ltr" },
+  { text: "مرحبًا، ", language: "Arabic", dir: "rtl" },
+  { text: "नमस्ते, ", language: "Hindi", dir: "ltr" },
+  { text: "Selam, ", language: "Turkish", dir: "ltr" },
+  { text: "Hai, ", language: "Malay", dir: "ltr" }
+]
+
+const container=document.querySelector('#salutation');
+let currindex=0;
+
+function typing(text, callback){
+    let index=0;
+    container.textContent='';
+    container.classList.add('typing');
+    function type(){
+        if(index<text.length){
+            container.textContent=text.slice(0,index+1);
+            index++;
+            setTimeout(type,100);
+        }
+        else{
+            container.classList.remove('typing');
+            setTimeout(callback, 1000);
+        }
+    }
+    type();
+}
+
+function cycle(){
+    const {text, dir}= translations[currindex];
+    typing(text,()=>{
+        container.classList.toggle('rtl', dir==='rtl');
+        currindex=(currindex+1) % translations.length;
+        cycle();
+    })
+}
+cycle();
